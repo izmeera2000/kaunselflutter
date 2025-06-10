f94175608616c89bccb02c88720979dbd53df4b9
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2025-06-10 22:41:41 +0800

    asdsa
    asd

--- ekaunsel/lib/screens/chatbot_page.dart
+++ ekaunsel/lib/screens/chatbot_page.dart
@@ -181,17 +181,15 @@ class _ChatbotPageState extends State<ChatbotPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text("Chat")),
+      ),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.only(left: 20, top: 20, right: 20),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              const Text(
-                'Chat',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
               const SizedBox(height: 10),
 
               // Chat History
@@ -215,34 +213,34 @@ class _ChatbotPageState extends State<ChatbotPage> {
                   },
                 ),
               ),
-Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          children: [
-            Expanded(
-              child: TextField(
-                controller: _controller,
-                decoration: InputDecoration(
-                  hintText: "Type a message",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: _controller,
+                        decoration: InputDecoration(
+                          hintText: "Type a message",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.send),
+                      onPressed: _sendMessage, // Send message on button click
+                    ),
+                  ],
                 ),
               ),
-            ),
-            IconButton(
-              icon: Icon(Icons.send),
-              onPressed: _sendMessage, // Send message on button click
-            ),
-          ],
-        ),
-      ),
               // Input Area
             ],
           ),
         ),
       ),
-     );
+    );
   }
 }
 
